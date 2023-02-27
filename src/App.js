bf72222b66fb9b6f0e3e7c90a13c6efcf10c8974
Anoop Santhanam <anoop.santhanam@gmail.com>
2023-02-27 11:55:35 +0530

    Coming soon page setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import styled from "styled-components";
 
-function App() {
+import Logo from "./assets/logo-min.png";
+
+const LogoHolder = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  margin-top: 128px;
+  text-align: center;
+`;
+
+const ComingSoonText = styled.p`
+  text-align: center;
+  font-weight: bold;
+  font-size: 32px;
+  color: #666;
+  text-transform: uppercase;
+  letter-spacing: -2px;
+  margin-top: 64px;
+  transition: all 0.2s ease;
+  transform: rotateX(30deg);
+`;
+
+const Footer = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: auto;
+  padding: 4px;
+  text-align: center;
+  background-color: #333;
+  color: #efefef;
+  font-size: 11px;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  color: #ffa500;
+`;
+
+const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      document.getElementById("soon-text").style.transform = "rotateX(0deg)";
+      document.getElementById("soon-text").style.color = "#999";
+    }, 1000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <LogoHolder>
+        <img src={Logo} alt="Meen Motors" width="40%" />
+      </LogoHolder>
+      <br />
+      <ComingSoonText id="soon-text">Coming soon</ComingSoonText>
+      <Footer>
+        Copyright &copy; {new Date().getFullYear()} &bull;{" "}
+        <Link href="https://www.auroradesignshq.com">Aurora Designs LLP</Link>{" "}
+        &bull; <Link href="https://www.auroradesignshq.com">Contact Us</Link>
+      </Footer>
+    </>
   );
-}
+};
 
 export default App;
